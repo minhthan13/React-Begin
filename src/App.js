b7fe66b18df5fa7ec256db18f094202d5ce63c0b
Krantz <nguyenminhthan1308@gmail.com>
2023-05-25 08:38:51 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { useDispatch, useSelector } from "react-redux";
-import { inceaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import "./sass/App.scss";
-import MyComponent from "./components/MyComponent";
-import React from "react";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <div className="app-container">hello world</div>;
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
