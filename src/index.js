7bbd0aa7e4d279c1ae55573d7a43a5da7bedc1d9
Krantz <nguyenminhthan1308@gmail.com>
2023-06-28 12:07:58 +0700

    use redux-persist config error f5 loading page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "nprogress/nprogress.css";
@@ -11,8 +12,10 @@ import Layout from "./Layout";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
