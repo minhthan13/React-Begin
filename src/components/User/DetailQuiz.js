a3386958d97db64f28dbe935a2110ad0d6247a00
Krantz <nguyenminhthan1308@gmail.com>
2023-07-03 14:23:22 +0700

    use react-roter-doom location state

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,45 +1,72 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
 import _ from "lodash";
+import "./DetailQuiz.scss";
 const DetailQuiz = (props) => {
-  const params = useParams();
-  const quizID = params.id;
+   const params = useParams();
+   const quizID = params.id;
+   const location = useLocation();
 
-  useEffect(() => {
-    fetchQuestion();
-  }, [quizID]);
-  const fetchQuestion = async () => {
-    let res = await getDataQuiz(quizID);
-    if (res && res.EC === 0) {
-      let raw = res.DT;
-      console.log("check raw data: ", raw);
-      let data = _.chain(raw)
-        .groupBy("id")
-        .map((value, key) => {
-          let answers = [];
-          let questionDescription,
-            image = null;
-          value.forEach((item, index) => {
-            if (index === 0) {
-              questionDescription = item.description;
-              image = item.image;
-            }
-            answers.push(item.answers);
-            // console.log("check item for each: ", item.answers);
-          });
-          console.log("value: ", value, "---key: ", key);
+   useEffect(() => {
+      fetchQuestion();
+   }, [quizID]);
+   const fetchQuestion = async () => {
+      let res = await getDataQuiz(quizID);
+      if (res && res.EC === 0) {
+         let raw = res.DT;
+         console.log("check raw data: ", raw);
+         let data = _.chain(raw)
+            .groupBy("id")
+            .map((value, key) => {
+               let answers = [];
+               let questionDescription,
+                  image = null;
+               value.forEach((item, index) => {
+                  if (index === 0) {
+                     questionDescription = item.description;
+                     image = item.image;
+                  }
+                  answers.push(item.answers);
+                  // console.log("check item for each: ", item.answers);
+               });
 
-          return { questionId: key, answers, questionDescription, image };
-        })
-        .value();
-      console.log(data);
-    }
-  };
-  return (
-    <div className="detail-quiz-container">
-      <span></span>detail quiz
-    </div>
-  );
+               return {
+                  questionId: key,
+                  answers,
+                  questionDescription,
+                  image,
+               };
+            })
+            .value();
+         console.log("data after lodash: ", data);
+      }
+   };
+   return (
+      <div className="detail-quiz-container">
+         <div className="left-content">
+            <div className="title">
+               Quiz {quizID}: {location?.state?.quizTitle}
+            </div>
+            <hr />
+            <div className="q-body">
+               <img src="" alt="" />
+            </div>
+            <div className="q-content">
+               <div className="question">Question 1: How are you doing </div>
+               <div className="answer">
+                  <div className="q-a">A. ashdj</div>
+                  <div className="q-a">B. isjadji</div>
+                  <div className="q-a">C. ishad i</div>
+               </div>
+            </div>
+            <div className="footer">
+               <button className="btn btn-primary ">Prev</button>
+               <button className="btn btn-secondary">Next</button>
+            </div>
+         </div>
+         <div className="right-content">count down</div>
+      </div>
+   );
 };
 export default DetailQuiz;
--- src/components/User/ListQuiz.js
+++ src/components/User/ListQuiz.js
@@ -10,7 +10,7 @@ const ListQuiz = (props) => {
   }, []);
   const getQuizData = async () => {
     const res = await getQuizByUser();
-    console.log("Check res: ", res);
+
     if (res && res.EC === 0) {
       setArrQuiz(res.DT);
     }
@@ -35,7 +35,11 @@ const ListQuiz = (props) => {
                 <p className="card-text">{quiz.description}</p>
                 <button
                   className="btn btn-primary"
-                  onClick={() => navigate(`/quiz/${quiz.id}`)}>
+                  onClick={() =>
+                    navigate(`/quiz/${quiz.id}`, {
+                      state: { quizTitle: quiz.description },
+                    })
+                  }>
                   Start Now
                 </button>
               </div>
